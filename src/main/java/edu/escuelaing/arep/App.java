a6f0f19b9a0d6a45f8b224b8f639dfde02ee41ec
Jose1102 <jose.castroortega1102@gmail.com>
2021-09-08 13:43:14 -0500

    Add repository and maven project

---
+++ src/main/java/edu/escuelaing/arep/App.java
@@ -0,0 +1,20 @@
+package edu.escuelaing.arep;
+
+
+import static spark.Spark.*;
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	
+    public static void main( String[] args ){
+    	
+    	System.out.println("Hello");
+    }
+    
+    
+    
+    
+    
+}
